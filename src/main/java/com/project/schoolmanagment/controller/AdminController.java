16557bbdb557a21b78cc0e19c873ecbab2f4f1fc
Cem Sahinkaya <mightyforever74@gmail.com>
2023-06-12 14:20:38 +0300

    my own work3 v2

---
+++ src/main/java/com/project/schoolmanagment/controller/AdminController.java
@@ -0,0 +1,22 @@
+package com.project.schoolmanagment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("admin")
+@RequiredArgsConstructor
+public class AdminController {
+    private final AdminService adminService;
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody @Valid AdminRequest adminRequest){
+        return ResponseEntity.ok(adminService.save(adminRequest));
+    }
+}
