a8984e5abb8e6c81567f4991ee25bae49fa874eb
Cem Sahinkaya <mightyforever74@gmail.com>
2023-06-21 00:50:57 +0300

    version 7.4

--- src/main/java/com/project/schoolmanagment/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/project/schoolmanagment/security/jwt/AuthTokenFilter.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,9 +28,9 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     private static final Logger LOGGER= LoggerFactory.getLogger(AuthTokenFilter.class);
 
 
-
+    @Autowired
     private  JwtUtils jwtUtils;
-
+    @Autowired
     private  UserDetailsServiceImpl userDetailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request,
