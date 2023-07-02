56ad215a6c46cd02901c6caf174c633b6ba56894
Cem Sahinkaya <mightyforever74@gmail.com>
2023-07-02 03:45:03 +0300

    version 13.3 messages 1-00

--- src/main/java/com/project/schoolmanagment/entity/concretes/Lesson.java
+++ src/main/java/com/project/schoolmanagment/entity/concretes/Lesson.java
@@ -1,15 +1,14 @@
 package com.project.schoolmanagment.entity.concretes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Data
@@ -17,7 +16,6 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 public class Lesson {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long lessonId;
@@ -25,13 +23,11 @@ public class Lesson {
     private String lessonName;
 
     private Integer creditScore;
-    /*
-    nicin int degil de Integer wrapperclass kullandik
-    avantaj 1 default degeri olmasin credit scor u dusunup gimemeiz gerekir yoksa null gelir
-    avantaj 2 wrapper class larin arkasinda metodlar var biraz daha erisimi kolay
 
-     */
-    private  Boolean isCompulsory;// zorulu derslerde birimi
+    private Boolean isCompulsory;
 
+    @JsonIgnore
+    @ManyToMany(mappedBy = "lesson")
+    private Set<LessonProgram> lessonPrograms;
 
 }
